181989aa882f767b745da0e9df7ff42247c85915
Ajay Thakur <ajaythk.94@gmail.com>
2024-09-02 19:49:45 +0545

    message_models_created

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'messages'
+        unique_together = (('sender', 'recipient'),)
+
+    def __str__(self):
+        return self.content
